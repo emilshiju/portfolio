cbccbab18aa7828465174880ffa03396e2c95133
emil shiju <emilshiju10@gmail.com>
2025-10-02 16:04:01 +0530

    added proper github link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,7 +36,7 @@ function Projects() {
               isBlog={false}
               title="NearbyChat"
               description="NearbyChat is a real-time video and chat platform connecting users globally using WebRTC and advanced matching. It features location-based search with Mapbox, push notifications, premium plans via Razorpay, and a TURN server on AWS, ensuring smooth connectivity and user engagement."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              ghLink="https://github.com/emilshiju/NearByChat_Backend"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
@@ -47,7 +47,7 @@ function Projects() {
               isBlog={false}
               title="Props24"
               description="Props24 is a real estate web app connecting users with suitable agents or agencies. It features profile verification, property listings, admin moderation, and a matching algorithm. Users can review agents, while admins manage verifications, cities, reports, and specializations."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/emilshiju/props_24"
               demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
@@ -58,7 +58,7 @@ function Projects() {
               isBlog={false}
               title="TechTrove"
               description="TechTrove is an E-commerce web app featuring secure Razorpay payments, OTP login via Twilio, email via Nodemailer, and powerful admin tools. It offers product, order, coupon, banner management, and is deployed on AWS EC2 with Route53 for scalability."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/emilshiju/TECH-TROVE"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
@@ -71,7 +71,7 @@ function Projects() {
               isBlog={false}
               title="Blog Application"
               description="A full-featured Blog Application where users can create, edit, update, and delete their own blog posts. It includes profile management and integrates Firebase for storing user images. This application offers a smooth and intuitive experience for personal content creation and management."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/emilshiju/Blog-Application"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
@@ -84,7 +84,7 @@ function Projects() {
               isBlog={false}
               title="Poll Application"
               description="Poll Application is a web platform that allows users to create public polls, vote, and engage in real-time discussions through a live chatbox. Anyone can participate, share opinions, and create their own polls to gather public feedback. It fosters open interaction and debate around each poll's options and topics."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/emilshiju/Poll-Application-Backend"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
@@ -97,7 +97,7 @@ function Projects() {
               isBlog={false}
               title="TODO App"
               description="A TODO App built with React and supporting libraries that allows users to add, edit, delete, and mark tasks as completed. It features internal state management and a background theme changer for a smooth, responsive, and customizable experience."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/emilshiju/To-Do-Application"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
@@ -110,7 +110,7 @@ function Projects() {
               isBlog={false}
               title="OLX"
               description="A user-friendly platform inspired by OLX for buying and selling products. Built with Firebase, it features Firestore for real-time database operations and Firebase Authentication for secure, seamless login and user management, ensuring a smooth and interactive marketplace experience."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/emilshiju/OLX-APPLICATION-"
               demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
@@ -122,7 +122,7 @@ function Projects() {
               isBlog={false}
               title="Netflix"
               description="A real-time Netflix clone built using React and the TMDB API for dynamic movie data. It features a sleek UI, live movie trailer playback using React Player, and provides an immersive video streaming experience similar to the original Netflix platform."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              ghLink="https://github.com/emilshiju/React-Netflix"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
@@ -133,7 +133,7 @@ function Projects() {
               isBlog={false}
               title="FirstStart"
               description="FirstStart is a website designed to highlight a startup's mission and vision. It showcases the startup’s goals and includes a contact form, making it easy for users to connect and engage with the team."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              ghLink="https://github.com/emilshiju/FIRST-START"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
